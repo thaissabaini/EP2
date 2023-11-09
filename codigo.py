68e17f8865103efd5ceb4d44a5ba5b6dc52220f4
carolinasilverio <carolinafms@al.insper.edu.br>
2023-11-09 17:53:28 -0300

    att2

--- codigo.py
+++ codigo.py
@@ -5,7 +5,7 @@ import random
 numero = 5
 filtradas = []
 
-print(' ===========================\n|                           |\n| Bem-vindo ao Insper Termo |')
+print(' ===========================\n|                           |\n| Bem-vindo ao Insper Termo |\n|                           |\n ==== Design de Software === \n\nComandos: desisto\n\n Regras:\n\n  - Você tem 6 tentativas para acertar uma palavra aleatória de 5 letras.\n  - A cada tentativa, a palavra testada terá suas letras coloridas conforme:\n    . Azul   : a letra está na posição correta;\n    . Amarelo: a palavra tem a letra, mas está na posição errada;\n    . Cinza: a palavra não tem a letra.\n  - Os acentos são ignorados;\n  - As palavras podem possuir letras repetidas.\n\nSorteando uma palavra...\nJá tenho uma palavra! Tente adivinhá-la!\n\nVocê tem 6 tentaviva(s)')
 def inidica_posicao(palavra_sorteada, palavra_especulada):
     palavra_especulada = palavra_especulada.lower()
     palavra_sorteada= palavra_sorteada.lower()
@@ -74,8 +74,8 @@ while jogando:
                 else:
                     jogando = 0
             else:
-                print('\nInsper :: TERMO')
-                print(f'Você tem {resta} tentativa(s)')
+                print('\nInsper :: TERMO\n')
+                print(f'Você tem {resta} tentativa (s)')
     else:
         print('Palavra já testada!')
         
