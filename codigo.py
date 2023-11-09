a815f5fecba229ab20aa93b9a42b4d24c9e0b757
thaissabaini <thaissc3@al.insper.edu.br>
2023-11-09 19:29:14 -0300

    finished the game

--- codigo.py
+++ codigo.py
@@ -1,11 +1,14 @@
 from lista import lista_palavras
 import random
+import colorama
+from colorama import init, Fore, Back, Style
 
+colorama.init()
 
 numero = 5
 filtradas = []
 
-print(' ===========================\n|                           |\n| Bem-vindo ao Insper Termo |\n|                           |\n ==== Design de Software === \n\nComandos: desisto\n\n Regras:\n\n  - Você tem 6 tentativas para acertar uma palavra aleatória de 5 letras.\n  - A cada tentativa, a palavra testada terá suas letras coloridas conforme:\n    . Azul   : a letra está na posição correta;\n    . Amarelo: a palavra tem a letra, mas está na posição errada;\n    . Cinza: a palavra não tem a letra.\n  - Os acentos são ignorados;\n  - As palavras podem possuir letras repetidas.\n\nSorteando uma palavra...\nJá tenho uma palavra! Tente adivinhá-la!\n\nVocê tem 6 tentaviva(s)')
+print(f' ===========================\n|                           |\n| Bem-vindo ao Insper Termo |\n|                           |\n ==== Design de Software === \n\nComandos: desisto\n\n Regras:\n\n  - Você tem {Fore.RED}6{Fore.RESET} tentativas para acertar uma palavra aleatória de 5 letras.\n  - A cada tentativa, a palavra testada terá suas letras coloridas conforme:\n    . {Fore.BLUE}Azul{Fore.RESET}   : a letra está na posição correta;\n    . {Fore.YELLOW}Amarelo{Fore.RESET}: a palavra tem a letra, mas está na posição errada;\n    . Branco : a palavra não tem a letra.\n  - Os acentos são ignorados;\n  - As palavras podem possuir letras repetidas.\n\nSorteando uma palavra...\nJá tenho uma palavra! Tente adivinhá-la!\n\nVocê tem 6 tentaviva(s)')
 def inidica_posicao(palavra_sorteada, palavra_especulada):
     palavra_especulada = palavra_especulada.lower()
     palavra_sorteada= palavra_sorteada.lower()
@@ -31,10 +34,21 @@ for palavra in lista_palavras:
 
 jogando = 1
 dic = {'n': len(filtradas[0]),  'tentativas': len(filtradas[0]) + 1, 'especuladas': [],  'sorteada': random.choice(filtradas)}
-print (dic['sorteada'])
+
+divisor = ' --- --- --- --- ---'
+linha1 ='|   |   |   |   |   |'
+linha2 ='|   |   |   |   |   |'
+linha3 ='|   |   |   |   |   |'
+linha4 ='|   |   |   |   |   |'
+linha5 ='|   |   |   |   |   |'
+linha6 ='|   |   |   |   |   |'
+
+linhas = [divisor, linha1, linha2, linha3, linha4, linha5, linha6]
+
 jogadas = 0
 while jogando:
     palavra_sorteada = dic['sorteada']
+
     palavra_especulada = str(input('Qual é seu palpite? '))
     if palavra_especulada == 'desisto':
         if input('Tem certeza que deseja desistir da rodada? [s|n] ') == 's':
@@ -54,16 +68,12 @@ while jogando:
     elif palavra_especulada not in dic['especuladas']:
         jogadas += 1
         dic['especuladas'].append(palavra_especulada)
-
         resta = int(len(filtradas[0]) + 1) - jogadas
         tentativa = inidica_posicao(palavra_sorteada, palavra_especulada)
-        j = 0
+        certo = 0
         for i in tentativa:
             if i == 0:
-                j += 1
-        if j == 5:
-            print(f'*** Parabéns! Você acertou após {jogadas} tentativas')
-            jogando = 0
+                certo += 1
         else:
             if resta == 0:
                 print('Perdeu')
@@ -73,9 +83,29 @@ while jogando:
                     jogadas = 0
                 else:
                     jogando = 0
+            print('\nInsper :: TERMO\n')
+            
+            for i in range (len(linhas)):
+                if i == jogadas:
+                    linha_nova = '| '
+                    for j in range (len(tentativa)):
+                        if tentativa[j] == 0:
+                            linha_nova += Fore.BLUE + f'{palavra_especulada[j]}' + Fore.RESET + ' | '
+                        elif tentativa[j] == 1:
+                            linha_nova += Fore.YELLOW + f'{palavra_especulada[j]}' + Fore.RESET+ ' | '
+                        else:
+                            linha_nova += f'{palavra_especulada[j]}' + ' | '
+                    linhas[i] = linha_nova
+                if i != 0:
+                    print(linhas[i])
+                print(divisor)
+            if certo == 5:
+                print(f'\n*** Parabéns! Você acertou após {jogadas} tentativa(s)')
+                jogando = 0
             else:
-                print('\nInsper :: TERMO\n')
-                print(f'Você tem {resta} tentativa (s)')
+                print(f'\nVocê tem {resta} tentativa (s)')
     else:
         print('Palavra já testada!')
+
+colorama.deinit()
         
